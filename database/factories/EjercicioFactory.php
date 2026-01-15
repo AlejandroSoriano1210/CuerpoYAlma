8b41fe0d6a032ba5c944adc771e0317de847da11
AlejandroSoriano1210 <alejandro.soriano@iesdonana.org>
2026-01-15 12:02:01 +0100

    seeders para bd

--- database/factories/EjercicioFactory.php
+++ database/factories/EjercicioFactory.php
@@ -17,7 +17,9 @@ class EjercicioFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nombre' => $this->faker->words(2, true),
+            'descripcion' => $this->faker->paragraph(),
+            'musculo_objetivo' => $this->faker->randomElement(['Pecho', 'Espalda', 'Piernas', 'Brazos', 'Hombros', 'Abdomen']),
         ];
     }
 }
--- database/seeders/HorarioTrabajoSeeder.php
+++ database/seeders/HorarioTrabajoSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\HorarioTrabajo;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,38 @@ class HorarioTrabajoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Crear 4 entrenadores
+        $entrenadores = User::factory(4)->create()->each(function ($user) {
+            $user->assignRole('entrenador');
+        });
+
+        // Horarios de trabajo para cada entrenador
+        $diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+        foreach ($entrenadores as $entrenador) {
+            // Cada entrenador trabaja 5-6 días
+            $diasAleatorios = array_slice($diasSemana, 0, rand(5, 6));
+
+            foreach ($diasAleatorios as $dia) {
+                HorarioTrabajo::create([
+                    'user_id' => $entrenador->id,
+                    'dia_semana' => $dia,
+                    'hora_inicio' => '06:00:00',
+                    'hora_fin' => '14:00:00',
+                ]);
+
+                // Algunos entrenadores tienen turno doble
+                if (rand(1, 100) <= 30) {
+                    HorarioTrabajo::create([
+                        'user_id' => $entrenador->id,
+                        'dia_semana' => $dia,
+                        'hora_inicio' => '14:00:00',
+                        'hora_fin' => '22:00:00',
+                    ]);
+                }
+            }
+        }
+
+        $this->command->info('✅ 4 entrenadores creados con sus horarios de trabajo.');
     }
 }
